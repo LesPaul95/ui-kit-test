a02601e5a1db97ac317840abf07a8565ac40c21b
Pavel Abakshin <p.abakshin@univertv.ru>
2020-12-16 17:08:47 +0300

    update theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -12,15 +12,37 @@ export const theme = createMuiTheme({
   },
   overrides: {
     MuiTypography: {
+      body1: {
+        fontFamily: 'Museo Sans, sans-serif',
+      },
+      body2: {
+        fontFamily: 'Museo Sans, sans-serif',
+      },
       h5: {
         fontWeight: 600,
+        fontFamily: 'Museo Sans, sans-serif',
+      },
+    },
+    MuiButton: {
+      root: {
+        fontFamily: 'Museo Sans, sans-serif',
       },
     },
     MuiInputBase: {
       input: {
+        fontFamily: 'Museo Sans, sans-serif',
         fontWeight: 300,
         letterSpacing: '0.04em',
         color: '#44464B',
+        '&::placeholder': {
+          color: '#92A7B6',
+          opacity: 1,
+        },
+      },
+      root: {
+        fontFamily: 'Museo Sans, sans-serif',
+        color: '#92A7B6',
+        opacity: 1,
       },
     },
     MuiLink: {
@@ -40,6 +62,39 @@ export const theme = createMuiTheme({
           borderBottom: '1px solid #1061D8',
         },
       },
+    },
+    MuiFormLabel: {
+      // @ts-ignore
+      width: '100%',
+      color: '#44464B',
+      fontFamily: 'Museo Sans, sans-serif',
+      fontWeight: 400,
+      fontSize: 18,
+      letterSpacing: '0.12em',
+      textAlign: 'left',
+      lineHeight: '22px',
+      disabled: {
+        color: '#44464B',
+      },
+      focused: {
+        color: '#44464B',
+      },
+      '@media (max-width: 1919px)': {
+        fontSize: 16,
+      },
+      '@media (max-width: 1199px)': {
+        fontSize: 16,
+        fontWeight: 600,
+      },
+      '@media (max-width: 639px)': {
+        fontSize: 12,
+        marginRight: -30,
+      },
+    },
+    MuiInputLabel: {
+      shrink: {
+        transform: 'none',
+      }
     },
   },
 });
